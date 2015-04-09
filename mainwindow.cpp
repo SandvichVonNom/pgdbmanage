1db3b5e6b36c30fd43fe697a1a3d378713aeedc0
Joshua Scheel <jscheel42@gmail.com>
2015-04-08 18:01:52 -0700

    Sample code for libexpect usage taken from "http://stackoverflow.com/ questions/13705869/why-do-i-get-a-segmentation-fault-in-my-simple-c- program-using-libexpect-so" to begin learning libexpect.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,9 @@
 #include <QStringList>
 #include <QDebug>
 #include <iostream>
+using std::cout;
+using std::endl;
+
 #include <string>
 #include <QFileDialog>
 #include <tcl.h>
@@ -105,4 +108,89 @@ void MainWindow::on_Backup_Btn_RunBackup_clicked()
 //    FILE* fd = exp_popen("ssh htpc@192.168.42.102");
 //    std::string host = "myhost";
 //    FILE *expect = exp_popen((char *) host.c_str());
+
+    // This section taken from "http://stackoverflow.com/questions/13705869/why-do-i-get-a-segmentation-fault-in-my-simple-c-program-using-libexpect-so" for learning purposes, will be changing almost everything once I better understand libexpect
+
+    enum{FOUNDSEARCH, PROMPT};
+    /* initialise expect and tcl */
+    Tcl_Interp *interp = Tcl_CreateInterp();
+
+    if(Tcl_Init(interp) == TCL_ERROR)
+    {
+        cout << "TCL failed to initialize." << endl;
+    }
+
+    if(Expect_Init(interp) == TCL_ERROR)
+    {
+        cout << "Expect failed to initialize." << endl;
+    }
+
+    /* end of intialisation procedure */
+
+    //open a shell with a pipe
+    char shellType[] = "sh";
+    FILE* fp = exp_popen(shellType);
+
+    //should we exit from the loop which is studying sh output
+    bool shouldBreak = false;
+    //did we find the pwd
+    bool foundSearch = false;
+    //does it look like expect is working
+    bool expectWorking = false;
+    //did we receive a prompt...therefore we should send a command
+    bool receivedPrompt = false;
+
+    while(shouldBreak == false)
+    {
+        switch(exp_fexpectl(fp,
+            exp_glob, "/tools/test*", FOUNDSEARCH,  //different
+            exp_glob,"# ", PROMPT, //cases are shown here
+            exp_end))  //that the expect loop could encounter
+        {
+            case FOUNDSEARCH:
+                foundSearch = true;
+                break;
+            case PROMPT:
+                if (receivedPrompt)
+                {
+                    shouldBreak = true;
+                    expectWorking = true;
+                }
+                else
+                {
+                    receivedPrompt = true;
+                    fprintf(fp, "%s\r", "pwd");
+                }
+                break;
+            case EXP_TIMEOUT:
+                shouldBreak = true;
+                break;
+            case EXP_EOF:
+                shouldBreak = true;
+                break;
+        }
+
+          //cout << "exp_match : " << exp_match << endl;
+    }
+
+    cout << endl;
+    if (foundSearch)
+    {
+        cout << "Expect found output of pwd" << endl;
+    }
+    else
+    {
+        cout << "Expect failed to find output of pwd" << endl;
+    }
+
+    if(expectWorking)
+    {
+        cout << "The expect interface is working" << endl;
+    }
+    else
+    {
+        cout << "The expect interface is not working" << endl;
+    }
+
+    cout << "The test program successfully reached the end" << endl;
 }
